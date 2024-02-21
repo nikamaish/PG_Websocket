d90868f93fe04cb778657224cf86731a1e370662
aish <aishnikam31@gmail.com>
2024-02-22 01:24:47 +0530

    simple msg sending

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const app = express();
 
 const http = require('http')
 
-
 const server = http.createServer(app);
 
-app.use (express.static('/public')); // why we wrote this line?  // to serve static files
+const { Server } = require('socket.io');
+// why we wrote this line? // to import the Server class from the socket.io package
+
+const io = new Server(server);
+
+// all the socket.io logic will go here and all the http request will handled by express
+
+io.on('connection',(socket)=>{
+    // console.log('A user connected', socket.id);
+
+    socket.on('user-message', (message)=>{
+        console.log('A new user message', message);
+    })
+})
+
+
+
+
+
+
+
+
+
+app.use (express.static('public')); // why we wrote this line?  // to serve static files
 // static files are files that don't change, such as images, CSS, and JavaScript files.
 
 const port = process.env.PORT || 5000;
