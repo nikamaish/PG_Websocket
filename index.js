c0f19066adcccd8b9f0339910ef8631f2300d2c8
aish <aishnikam31@gmail.com>
2024-02-23 01:45:54 +0530

    some imp understanding of code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 
+// Your server-side code
 const http = require("http");
 const express = require("express");
 const path = require("path");
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Keep track of connected users
+// Keep track of connected users and their socket IDs
 const users = new Map();
 
 // Socket.io
@@ -22,11 +22,12 @@ io.on("connection", (socket) => {
     const { recipient, message } = data;
     const sender = users.get(socket.id);
 
-    // Send the message only to the recipient
     const recipientSocketId = Array.from(users.entries()).find(([id, name]) => name === recipient)?.[0];
     if (recipientSocketId) {
-      io.to(recipientSocketId).emit("message", { sender, message });
+      io.to(recipientSocketId).emit("message", { sender, recipient, message });
     }
+
+    io.to(socket.id).emit("message", { sender, recipient, message });
   });
 
   socket.on("disconnect", () => {
@@ -42,43 +43,3 @@ app.get("/", (req, res) => {
 });
 
 server.listen(9000, () => console.log(`Server Started at PORT:9000`));
-
-
-
-
-
-
-
-
-// 
-// const http = require("http");
-// const express = require("express");
-// const path = require("path");
-// const { Server } = require("socket.io");
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// // Socket.io
-// io.on("connection", (socket) => {
-//   socket.on("user-message", (message) => {
-//     io.emit("message", message);
-//   });
-// });
-
-// app.use(express.static(path.resolve("./public")));
-
-// app.get("/", (req, res) => {
-//   return res.sendFile("/public/index.html");
-// });
-
-// server.listen(9000, () => console.log(`Server Started at PORT:9000`));
-
-
-
-
-
-
-
-
